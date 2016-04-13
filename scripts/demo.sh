9c3d3ea28106a885f811d50b846c18172720461b
Evan Summers <evan.summers@gmail.com>
2016-04-13 22:00:03 +0200

    deinit

--- scripts/demo.sh
+++ scripts/demo.sh
@@ -6,20 +6,10 @@ rediscli() {
   redis-cli -n 13 $*
 }
 
-  if [ `hostname -s` = 'eowyn' -a $USER = 'evans' -a `date +%s` -lt 1460056666 ]
-  then
-    for key in `rediscli keys 'demo:hmsetprops:*'`
-    do
-      rediscli del $key
-    done
-  fi
+[ `hostname -s` = 'eowyn' -a $USER = 'evans' ]
+
   npm run demo 
-  if [ `hostname -s` = 'eowyn' -a $USER = 'evans' -a `date +%s` -lt 1460056666 ]
-  then
-    rediscli keys 'demo:hmsetprops:*' 
-    rediscli hgetall demo:hmsetprops:1
-    rediscli hgetall demo:hmsetprops:1:audit
-    rediscli get demo:hmsetprops:id
-    rediscli smembers demo:hmsetprops:ids
-  fi
+
+  rediscli keys 'demo:hmsetprops:*' 
+  rediscli hgetall 'demo:hmsetprops:1'
 
