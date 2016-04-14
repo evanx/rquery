bdf3d7876a9d24b7937810073617d921cb590e9d
Evan Summers <evan.summers@gmail.com>
2016-04-14 17:57:46 +0200

    pm2

--- scripts/start.pm2.sh
+++ scripts/start.pm2.sh
@@ -1,6 +1,6 @@
 
 
-  NODE_ENV=production configModule=./demo.js redisUrl='redis://localhost:6379/13' pm2 start --name rquery
+  NODE_ENV=production configModule=./demo.js redisUrl='redis://localhost:6379/13' pm2 start index.js --name rquery
 
   ls -l ~/.pm2/logs/rquery*
 
