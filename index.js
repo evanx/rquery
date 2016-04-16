4813efdeab95e61fdcb9229d6d36832141b3d593
Evan Summers <evan.summers@gmail.com>
2016-04-16 11:21:09 +0200

    SupervisorStarter

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
 
-// create context for Supervisor and components
-
-require('./lib/_entry.js');
+require('babel-polyfill');
+require('babel-core/register');
+require('./lib/SupervisorStarter.js').startSupervisor();
