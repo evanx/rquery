97be95bbe1a6649cace426977036ea0426706f60
Evan Summers <evan.summers@gmail.com>
2016-04-28 17:36:44 +0200

    ssl

--- src/rquery.js
+++ src/rquery.js
@@ -759,7 +759,8 @@ export default class {
    }
 
    validateAccess(req, options, keyspace, token, accessToken, readToken, certs) {
-      logger.debug('validateAccess', req.get('X-Forwarded-Proto'), keyspace);
+      const scheme = req.get('X-Forwarded-Proto');
+      logger.debug('validateAccess', scheme, keyspace);      
       if (options.command.key === 'importcerts') {
          logger.info('validateAccess importcerts', keyspace);
       } else if (config.secureDomain || this.isSecureDomain(req)) {
