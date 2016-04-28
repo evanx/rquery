375b025611b3b83c372a1cf2452178a1f14a61dd
Evan Summers <evan.summers@gmail.com>
2016-04-28 14:59:19 +0200

    scripts

--- scripts/debug.start.pm2.sh
+++ scripts/debug.start.pm2.sh
@@ -13,6 +13,11 @@ then
   port=8766
 fi
 
+git pull
+git submodule update
+
+pm2 delete $name
+
 rquery_port=$port NODE_ENV=production loggerLevel=debug configModule=./demo.js redisUrl='redis://localhost:6379/13' pm2 start index.js --name $name
 
 sleep 1
