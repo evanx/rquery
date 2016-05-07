e8ed0c912ebf9a98c98fb82aec1a2962190fe546
Evan Summers <evan.summers@gmail.com>
2016-05-08 01:39:27 +0200

    register-account

--- src/rquery.js
+++ src/rquery.js
@@ -38,6 +38,7 @@ export default class {
 
    async sendErrorRoute(req, res) {
       const [account, keyspace] = Strings.matches(req.path, /^\/ak\/([a-z])([a-z]+)\/([^\/]+)\//);
+      logger.debug('sendErrorRoute', account, keyspace, this.isBrowser(req));
       if (account && keyspace) {
          if (this.isBrowser(req)) {
             res.redirect(302, ['/ak', account, keyspace, 'help'].join('/'));
