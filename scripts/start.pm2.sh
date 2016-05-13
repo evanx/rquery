4445270c4bf0fde87c36ea9071e09558903be6c5
Evan Summers <evan.summers@gmail.com>
2016-05-13 04:56:19 +0200

    start

--- scripts/start.pm2.sh
+++ scripts/start.pm2.sh
@@ -16,7 +16,7 @@ rquery_botUrl=$botUrl rquery_botSecret=$botSecret rquery_port=$port NODE_ENV=pro
 (
   sleep 1
   ls -l ~/.pm2/logs/${name}*
-  log=`ls -t ~/.pm2/logs/${name}-output-* | head -1`
+  log=`ls -t ~/.pm2/logs/${name}-out-* | head -1`
   echo log $log
   tail -f $log | sed '/ started/q'
 )
