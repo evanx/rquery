67796c405c8f5d79133aaf72ed75dafb114205d2
Evan Summers <evan.summers@gmail.com>
2016-05-13 04:54:18 +0200

    start

--- scripts/start.pm2.sh
+++ scripts/start.pm2.sh
@@ -12,6 +12,13 @@ echo botUrl $botUrl
 rquery_botUrl=$botUrl rquery_botSecret=$botSecret rquery_port=$port NODE_ENV=production loggerLevel=debug configModule=./config/${config}.js \
   pm2 start index.js --name $name
 
-sleep 1
-ls -l ~/.pm2/logs/${name}*
+
+(
+  sleep 1
+  ls -l ~/.pm2/logs/${name}*
+  log=`ls -t ~/.pm2/logs/${name}-output-* | head -1`
+  echo log $log
+  tail $log | sed '/ started/q'
+)
+
 
