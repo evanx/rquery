c04c9ac24f25a5b3eb820ffcdb9c9193665f321e
Evan Summers <evan.summers@gmail.com>
2016-05-24 23:55:17 +0200

    scripts

--- scripts/show.nginx.sh
+++ scripts/show.nginx.sh
@@ -1,6 +1,5 @@
 
-  cat /nginx-local/sites/redishub.com
-  echo
-  cat /nginx-local/sites/redishub.com | grep -v '^\s*#' | grep 'redishub'
+
+  #cat /nginx-local/sites/redishub.com | grep -v '^\s*#' | grep 'redishub'
 
   ls -l /nginx-local/routes/redishub/* | grep '^l'
