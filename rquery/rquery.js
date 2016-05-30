995496bf06e872cc2c0bf8d2171bd181653f1b7d
Evan Summers <evan.summers@gmail.com>
2016-05-30 20:00:37 +0200

    telegram

--- rquery/rquery.js
+++ rquery/rquery.js
@@ -522,6 +522,10 @@ export default class {
             `  openssl x509 -text -in privcert.pem | grep 'CN=' &&`,
             `  curl -s -E privcert.pem ${this.config.hostUrl}/register-account-telegram/${account} &&`,
             `  echo 'Registered account ${account} OK'`,
+            `  echo 'Try the following:'`,
+            `  echo 'cd && git clone git@github.com:evanx/redishub.git && cd redishub &&'`,
+            `  echo 'alias rh=~/redishub/bin/rhcurl'`,
+            `  echo 'rh help'`,
             ``
          ].join('\n');
       });
