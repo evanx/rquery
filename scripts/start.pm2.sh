fbae8825d496d485faec98483dd3acde99d1a181
Evan Summers <evan.summers@gmail.com>
2016-06-01 01:24:14 +0200

    script

--- scripts/start.pm2.sh
+++ scripts/start.pm2.sh
@@ -3,6 +3,26 @@ set -u -e
 
 . ./scripts/_name.sh
 
+serviceId=`basename $PWD`
+if [ $serviceId -gt 0 ]
+then
+  name=$serviceId-$config
+fi
+
+# bot 
+
+botName=redishub
+if [ $config = test ]
+then
+  botName=rhtest
+elif [ $config = demo ]
+then
+  botName=rhdemo
+fi
+echo botName $botName
+botSecret=`cat ~/.bot.$botName/secret`
+botUrl=`cat ~/.bot.$botName/url`
+
 rquery_botUrl=$botUrl rquery_botSecret=$botSecret rquery_port=$port \
   NODE_ENV=production loggerLevel=debug configModule=./config/${config}.js \
   pm2 start index.js --name $name
