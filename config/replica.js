791a999136c65fd74802b5de4ad2cb575724d76b
Evan Summers <evan.summers@gmail.com>
2016-06-01 01:38:27 +0200

    replica config

---
+++ config/replica.js
@@ -0,0 +1,33 @@
+
+module.exports = {
+   components: {
+      rquery: {
+         redisUrl: 'redis://localhost:6679/1',
+         redisKeyspace: 'demo:rquery',
+         hostname: 'secure.redishub.com',
+         hostUrl: 'https://secure.redishub.com',
+         serviceName: 'redishub-prod',
+         serviceLabel: 'RedisHub',
+         port: 4567,
+         location: '',
+         assetsUrl: '/assets',
+         aboutUrl: 'https://github.com/evanx/redishub/blob/master/README.md',
+         helpUrl: 'https://github.com/evanx/redishub/blob/master/README.md',
+         indexCommand: 'show-keyspace-config',
+         //keyExpire: 2764800, // 31 days
+         enrollExpire: 300,
+         keyExpire: 604800, // 7 days
+         ephemeralKeyExpire: 600,
+         ephemeralAccountExpire: 1200,
+         certLimit: 4,
+         secureDomain: true,
+         cliDomain: false,
+         htmlDomain: true,
+         registerLimit: 10,
+         importLimit: 5,
+         adminLimit: 1,
+         defaultFormat: 'html',
+         addClientIp: false
+      }
+   }
+};
