3b049eca8efc17f34b778f06bb9b930d1bdd9438
Evan Summers <evan.summers@gmail.com>
2016-06-01 19:59:30 +0200

    open

--- config/test.js
+++ config/test.js
@@ -5,7 +5,7 @@ module.exports = {
          redisUrl: 'redis://localhost:6379/14',
          redisKeyspace: 'test:rquery',
          openHostname: 'test.redishub.com',
-         hostname: 'test.redishub.com',
+         hostname: 'redishub.com',
          hostUrl: 'https://test.redishub.com',
          serviceName: 'redishub-test',
          serviceLabel: 'RH Test',
