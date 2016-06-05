e8d0ffd592f94d86b08dc390649611514f81e075
Evan Summers <evan.summers@gmail.com>
2016-06-05 05:12:33 +0200

    html

--- scripts/lib.commit.sh
+++ scripts/lib.commit.sh
@@ -8,12 +8,12 @@ pwd
 
 c1commit() {
   message="$1"
-  git remote set-url origin git@github.com:evanx/libv.git
+  git remote set-url origin git@github.com:evanx/rhlibv.git
   git add -A
   git commit -m "$message" || echo "commit exit code $?"
   git remote -v 
   git push
-  git remote set-url origin https://github.com/evanx/libv.git
+  git remote set-url origin https://github.com/evanx/rhlibv.git
   echo; echo "done lib"
   git status | sed '/^$/d'
   echo; echo "sync"
