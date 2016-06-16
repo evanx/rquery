1a1eef8ed975f3c2215529b62fbf8d4120e31c56
Evan Summers <evan.summers@gmail.com>
2016-06-16 02:31:56 +0200

    redishub bot

--- rquery/handlers/certScript.js
+++ rquery/handlers/certScript.js
@@ -82,45 +82,11 @@ export default async function handleCertScript(req, res, reqx, {config}) {
       ]);
    }
    result = result.concat([
-      ``,
-      `  # TODO curl following from static stable versioned script from https://raw.githubusercontent.com/webserva/home`,
-      `  if [ ! -d \${dir} ]`,
-      `  then`,
-      `    echo "Directory \${dir} already exists. Try add '?archive' query to the URL."`,
-      `  else # directory does not exist`,
-      `    mkdir \${dir}`,
-      `    cd $_`,
-      `    echo "\${account}" > account`,
-      `    if openssl req -x509 -nodes -days 365 -newkey rsa:2048 \\`,
-      `      -subj "/CN=\${CN}/OU=\${OU}/O=\${O}" \\`,
-      `      -keyout privkey.pem -out cert.pem`,
-      `    then`,
-      `      openssl x509 -text -in cert.pem > x509.txt`,
-      `      grep 'CN=' x509.txt`,
-      `      cat cert.pem | head -3 | tail -1 | tail -c-12 > cert.extract.pem`,
-      `      cat privkey.pem cert.pem > privcert.pem`,
-      `      openssl x509 -text -in privcert.pem | grep 'CN='`,
-      `      curl -s -E privcert.pem "$certWebhook" ||`,
-      `        echo "Registered account \${account} ERROR $?"`,
-      `      if ! openssl pkcs12 -export -out privcert.p12 -inkey privkey.pem -in cert.pem`,
-      `      then`,
-      `        echo; pwd; ls -l`,
-      `        echo "ERROR $?: openssl pkcs12 ($PWD)"`,
-      `        false # error code 1`,
-      `      else`,
-      `        echo "Exported $PWD/privcert.p12 OK"`,
-      `        pwd; ls -l`,
-      `        sleep 2`,
-      ]);
-      result = result.concat(help.map(line => `        echo "${line}"`));
-      result = result.concat([
-      `        curl -s https://raw.githubusercontent.com/webserva/home/master/docs/install.rhcurl.txt`,
-      `        certExtract=\`cat cert.extract.pem\``,
-      `        echo "Try https://telegram.me/redishub_bot '/grantcert \$certExtract'"`,
-      `      fi`,
-      `    fi`,
-      `  fi`,
-      ')',
+      `  curl -s https://raw.githubusercontent.com/evanx/redishub/bin/cert-script.sh`,
+      `  echo 'Press Ctrl-C to abort, Enter to execute'`,
+      `  read _continue`,
+      `  curl -s https://raw.githubusercontent.com/evanx/redishub/bin/cert-script.sh | bash`,
+      `)`,
    ]);
    result.push('');
    return lodash.flatten(result);
