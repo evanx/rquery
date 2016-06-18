2040035e5a7e093f4a72c1a0d9b6dfbee6cec816
Evan Summers <evan.summers@gmail.com>
2016-06-18 16:55:20 +0200

    cert script sha1sum

--- scripts/sha1sum.cert-script.sh
+++ scripts/sha1sum.cert-script.sh
@@ -1,8 +1,8 @@
 
 set -e -u
 
-curl -s https://raw.githubusercontent.com/evanx/redishub/master/bin/cert-script.sh | sha1sum > assets/cert-script.sh.sha1sum
-curl -s https://raw.githubusercontent.com/evanx/redishub/master/bin/cert-script.sh > tmp/cert-script.sh
+curl -s https://raw.githubusercontent.com/webserva/webserva/master/bin/cert-script.sh | sha1sum > assets/cert-script.sh.sha1sum
+curl -s https://raw.githubusercontent.com/webserva/webserva/master/bin/cert-script.sh > tmp/cert-script.sh
 ls -l assets/cert-script.sh.sha1sum
 cat assets/cert-script.sh.sha1sum
 sha1sum tmp/cert-script.sh
