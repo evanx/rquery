e9fc8494223a4fb8decb5ba429b4a0faaffe3b58
Evan Summers <evan.summers@gmail.com>
2016-06-21 00:47:05 +0200

    cert-script

--- assets/cert-script.sh
+++ assets/cert-script.sh
@@ -1,34 +1,26 @@
-  if [ ! -d ${dir} ]
+  echo "${account}" > account
+  if openssl req -x509 -nodes -days 365 -newkey rsa:2048 \
+    -subj "/CN=${CN}/OU=${OU}/O=${O}" \
+    -keyout privkey.pem -out cert.pem
   then
-    echo "Directory ${dir} already exists. Try add '?archive' query to the URL."
-  else # directory does not exist
-    mkdir ${dir}
-    cd $_
-    echo "${account}" > account
-    if openssl req -x509 -nodes -days 365 -newkey rsa:2048 \
-      -subj "/CN=${CN}/OU=${OU}/O=${O}" \
-      -keyout privkey.pem -out cert.pem
+    openssl x509 -text -in cert.pem > x509.txt
+    grep 'CN=' x509.txt
+    echo -n `cat cert.pem | head -n-1 | tail -n+2` | sed -e 's/\s//g' | sha1sum | cut -f1 -d' ' > cert.pem.sha1sum
+    cat privkey.pem cert.pem > privcert.pem
+    openssl x509 -text -in privcert.pem | grep 'CN='
+    curl -s -E privcert.pem "$certWebhook" ||
+      echo "Registered account ${account} ERROR $?"
+    if ! openssl pkcs12 -export -out privcert.p12 -inkey privkey.pem -in cert.pem
     then
-      openssl x509 -text -in cert.pem > x509.txt
-      grep 'CN=' x509.txt
-      cat cert.pem | head -3 | tail -1 | tail -c-12 > cert.extract.pem
-      cat privkey.pem cert.pem > privcert.pem
-      openssl x509 -text -in privcert.pem | grep 'CN='
-      curl -s -E privcert.pem "$certWebhook" ||
-        echo "Registered account ${account} ERROR $?"
-      if ! openssl pkcs12 -export -out privcert.p12 -inkey privkey.pem -in cert.pem
-      then
-        echo; pwd; ls -l
-        echo "ERROR $?: openssl pkcs12 ($PWD)"
-        false # error code 1
-      else
-        echo "Exported $PWD/privcert.p12 OK"
-        pwd; ls -l
-        sleep 2
-        curl -s https://webserva.com/cert-script-help/${account}
-        curl -s https://raw.githubusercontent.com/webserva/webserva/master/docs/install.wscurl.txt
-        certExtract=`cat cert.extract.pem`
-        echo "Try https://telegram.me/$telegramBot?start '/grantcert $certExtract'"
-      fi
+      echo "ERROR $?: openssl pkcs12 ($PWD)"
+      false # error code 1
+    else
+      echo "Exported $PWD/privcert.p12 OK"
+      pwd; ls -l
+      sleep 2
+      curl -s https://open.webserva.com/cert-script-help/${account}
+      curl -s https://raw.githubusercontent.com/webserva/webserva/master/docs/install.wscurl.txt
+      certSha=`cat cert.pem.sha1sum`
+      echo "Try '/grantcert $certSha' via https://telegram.me/WebServaBot?start"
     fi
   fi
--- rquery/handlers/certScript.js
+++ rquery/handlers/certScript.js
@@ -110,7 +110,7 @@ export async function handleCertScript(req, res, reqx, {config}) {
       `    echo 'Double checking script integrity hashes:'`,
       `    sha1sum cert-script.sh # double check its SHA against another source below`,
       `    curl -s https://open.webserva.com/assets/cert-script.sh.sha1sum`,
-      `    echo 'f6edc446466e228965e51bee120425b497605949' # hardcoded SHA of stable version`,
+      `    echo '184063fdc03f1b2ee3781d24dd8ace859bd86f89' # hardcoded SHA of stable version`,
       `    echo 'Press Ctrl-C in the next 8 seconds to abort, and if any of the above hashes differ'`,
       `    sleep 8 # give time to abort if SHAs not consistent, or script review incomplete`,
       `    source <(cat cert-script.sh) # execute fetched script, hence the above review and SHA`,
