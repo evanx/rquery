e78c9d1f276f082ec209322f345f287965b60de8
Evan Summers <evan.summers@gmail.com>
2016-11-07 14:49:54 +0000

    login

--- scripts/del.all.login.sh
+++ scripts/del.all.login.sh
@@ -1,4 +1,10 @@
 
+  for key in `redis-cli -n 1 keys 'rquery:login:*'`
+  do 
+    echo $key 
+    redis-cli -n 1 del $key 
+  done
+
   for key in `redis-cli -n 1 keys 'rquery:session:*'`
   do 
     echo $key 
